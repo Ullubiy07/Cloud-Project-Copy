490784bf67835e1c212c17f40d260e47eb0addaa
Kozinov Sergey <sesekozinov@edu.hse.ru>
2026-03-26 01:21:01 +0300

    update sql

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,9 @@
 export const LANGUAGES = {
     python: "3.12",
     javascript: "18.19.0",
-    c: "",
+    c: "latest",
     assembly: "NASM",
-    sql: "",
+    sql: "latest",
     cpp: "C++20",
 };
 
@@ -13,7 +13,7 @@ export const SNIPPETS = {
     cpp: `// Here you can write your code\n#include <iostream>\n\nint main() {\n\tstd::cout << "Hello, Cloud Computing!";\n\treturn 0;\n}`,
     c: `// Here you can write your code\n#include <stdio.h>\n\nint main(void) {\n\tprintf("Hello, Cloud Computing!");\n\treturn 0;\n}`,
     assembly: `; Here you can write your code\nsection .data\n\tmsg db 'Hello, World!', 0\n\nsection .text\n\textern io_print_string\n\textern io_newline\n\tglobal main\n\nmain:\n\tpush rbp\n\tmov rbp, rsp\n\t\n\n\tmov rdi, msg\n\tcall io_print_string\n\tcall io_newline\n\n\tmov rsp, rbp\n\tpop rbp\n\txor eax, eax\n\tret\n`,
-    sql: ``,
+    sql: `CREATE TABLE users (\n\tid INTEGER PRIMARY KEY AUTOINCREMENT,\n\tname TEXT NOT NULL,\n\temail TEXT UNIQUE,\n\tcreated_at DATETIME DEFAULT CURRENT_TIMESTAMP\n);\n\nINSERT INTO users (name, email)\nVALUES\n\t('Alice', 'alice@example.com'),\n\t('Bob', 'bob@example.com'),\n\t('Charlie', 'charlie@example.com');\n\nSELECT * FROM users;\n`,
 }
 
 export const DEFAULT_FILES = {
